54efbdb9fc2a2eb7ad7449a26bfb26bb27d97dc5
zainahmed0597 <zainahmed0597@gmail.com>
2023-11-02 22:16:41 +0500

    First commit

---
+++ lib/ui/views/home/home_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+import 'home_viewmodel.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<HomeViewModel>.reactive(
+      viewModelBuilder: () => HomeViewModel(),
+      builder: (context, model, child) => Scaffold(
+        body: Center(
+          child: Text(model.title),
+        ),
+      ),
+    );
+  }
+}
